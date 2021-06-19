b0f2bc1decfa14c3361b9a473735c29313740281
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-06-19 16:09:29 +0200

    preparazione

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,20 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+    <title>Document</title>
 
-   @yield('header_scripts')
+    @yield('header_scripts')
 </head>
-
 <body>
-    @include('partials.header')
-
-    @yield('content')
-
+     @include('partials.header')
 </body>
-
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,63 @@
+<header>
+    <div class="head-credits">
+        <ul>
+            <li><a href="">DC POWER  VISA</a></li>
+            <li><a href="">ADDITIONAL DC SITIES <i class="fa fa-caret-down"></a></i></li>
+        </ul>
+    </div>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="{{ url('/')}}">
+
+                    <div class="logo">
+                        <img src="{{ asset ('images/dc-logo.png') }}" alt="dc-logo">
+                    </div>
+
+                </a>
+            </li>
+
+            <li>
+                <a href="">CHARACTERS</a>
+            </li>
+
+            <li>
+                <a href="{{ route('comics')}}">COMICS</a>
+            </li>
+
+            <li>
+                <a href="">TV</a>
+            </li>
+
+            <li>
+                <a href="">GAMES</a>
+            </li>
+
+            <li>
+                <a href="">COLLECTIBLES</a>
+            </li>
+
+            <li>
+                <a href="">VIDEOS</a>
+            </li>
+
+            <li>
+                <a href="">FANS</a>
+            </li>
+
+            <li>
+                <a href="">NEWS</a>
+            </li>
+
+            <li>
+                <a href="">SHOP</a>
+            </li>
+
+            <li>
+                <input type="text" placeholder="Search">
+            </li>
+        </ul>
+    </nav>
+
+</header>
