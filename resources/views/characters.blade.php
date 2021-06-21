b455c6306ee83bbb328f701ec3837fa410396841
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-06-21 16:23:48 +0200

    modifica main icons

--- resources/views/characters.blade.php
+++ resources/views/characters.blade.php
@@ -32,10 +32,10 @@
             <img src="{{asset('images/buy-comics-merchandise.png')}}" alt="merchandise">
             <h5>DC MERCHANDISE</h5>
 
-            <img src="{{asset('images/buy-comics-shop-locator.png')}}" alt="shop-locator">
+            <img src="{{asset('images/buy-comics-subscriptions.png')}}" alt="shop-locator">
             <h5>SUBSCRIPTIONS</h5>
 
-            <img src="{{asset('images/buy-comics-subscriptions.png')}}" alt="subscriptions">
+            <img src="{{asset('images/buy-comics-shop-locator.png')}}" alt="subscriptions">
             <h5>COMIC SHOP LOCATOR</h5>
 
             <img src="{{asset('images/buy-dc-power-visa.svg')}}" alt="power-visa">
