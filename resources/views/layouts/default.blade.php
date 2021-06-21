b8ac3cd52662854cd95b1470305b1d6c4ab5577c
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-06-21 15:06:28 +0200

    modifica homepage

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -19,8 +19,9 @@
             <h1>WELCOME TO DC</h1>
         </div>
     </div>
-    <section class="homepage">
 
+    <section class="characters-container">
+        <img src="{{asset('images/homepage.jpg')}}" alt="homepage">
     </section>
 
 
